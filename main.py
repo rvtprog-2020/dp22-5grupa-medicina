035bfaa2d05f36319eb7f286b2ea187ec1811755
Dima Vaseckins <dima.vaseckins@inbox.lv>
2020-11-27 15:44:27 +0200

    flask commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    return render_template('Page1.html')
+    
+@app.route('/pieteikties')
+def pieteikties():
+    return render_template('pieteiktViziti.html')
+
+@app.route('/manasp')
+def manaspieties():
+    return render_template('jusuVizite.html')
+
+@app.route('/register')
+def register():
+    return render_template('SecondPage.html')
+
+
+app.run(host='0.0.0.0', port=80, debug=True)
